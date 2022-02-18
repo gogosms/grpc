a34161acc6c3737cd616274391b76b10898b9afa
Rodrigo Valdez <gogosms@gmail.com>
2022-02-18 15:18:43 -0300

    Add gRPC.ClientConsole

---
+++ src/gRPC.ClientConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using gRPC.GrpcService;
+using Grpc.Net.Client;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7016/");
+var client = new Greeter.GreeterClient(channel);
+
+var response = await client.SayHelloAsync(new HelloRequest { Name = "Rodrigo" });
+
+Console.WriteLine(response.Message);
+Console.ReadLine();
